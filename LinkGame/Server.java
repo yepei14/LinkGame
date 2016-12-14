9fca002ba51df93521c87dde0c723ba13da429b3
DESKTOP-GKG54A0\Pi <yepei14@outlook.com>
2016-12-14 09:56:38 +0800

    本地游戏基本完成作业功能，网络通信方面存在较多问题

---
+++ LinkGame/Server.java
@@ -0,0 +1,20 @@
+package LinkGame;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+//³ÌÐòÈë¿Ú
+public class Server {
+   public static void main(String[] agrs) throws MalformedURLException{
+         ServerAddressFrame addressFrame = new ServerAddressFrame();  
+   }  
+}
+
