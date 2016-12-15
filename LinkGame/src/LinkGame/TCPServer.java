b5e7ce6cd56cc082fe7748fb8aa01be2f6b1b0ce
DESKTOP-GKG54A0\Pi <yepei14@outlook.com>
2016-12-15 15:27:08 +0800

    为了导出可执行jar文件创建了eclipse工程 路径什么的简直了

---
+++ LinkGame/src/LinkGame/TCPServer.java
@@ -0,0 +1,56 @@
+﻿package LinkGame;
+
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
+import java.lang.*;
+import org.omg.PortableInterceptor.INACTIVE;
+
+
+public class TCPServer extends Thread{
+  private LinkGame game;
+  InetAddress ip;
+  int host;
+  boolean flag = true; // 用于记录服务端的客户端是否已开启
+  public void setGame(LinkGame game){
+    this.game = game;
+  }
+
+  TCPServer(InetAddress inIp, int inHost){
+    ip = inIp;
+    host = inHost;
+  }
+
+  @Override
+  public void run(){
+    String strSocket, strLocal;
+    try {
+      ServerSocket ssocketWelcome = new ServerSocket(host);
+      while(true) {
+        Socket socketServer = ssocketWelcome.accept();
+        BufferedReader brInFromClient = new BufferedReader(new InputStreamReader(socketServer.getInputStream()));
+        do{
+          strSocket = brInFromClient.readLine();
+          if (!flag){
+            game.tcpServer(strSocket);
+          }
+          else{
+            // 客户端第一次发送消息
+            flag = false;
+            if (strSocket.equals("client")){
+              game.createClient(ip, 6789);
+              game.dosOutToServer.writeBytes(game.packState() + '\n');
+            }
+            else {
+              game.isTransport = true;
+              game.parseState(strSocket);
+              game.init();
+            }
+          }
+        } while(true);
+    }
+   }
+   catch (Exception e){
+   }
+  }
+}
